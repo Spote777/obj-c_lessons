63510807471e7b7407d17bfdeeecb7d6d4656116
Spote777 <66115092+Spote777@users.noreply.github.com>
2021-04-25 09:45:48 +0300

    lesson2

---
+++ lesson2/lesson2/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  lesson2
+//
+//  Created by Павел Заруцков on 25.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ lesson2/lesson2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson2
+//
+//  Created by Павел Заруцков on 25.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
