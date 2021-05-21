000769480dfd5c18e55c13d64a9f5631e9051374
Spote777 <66115092+Spote777@users.noreply.github.com>
2021-05-21 21:41:18 +0300

    lesson4

---
+++ lesson1/lesson4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lesson4
+//
+//  Created by Павел Заруцков on 21.05.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
